f87ba72215aae33e1efeff8e8d8fd880161ca5fb
vinodpatil2002 <vinodnspatil@gmail.com>
2025-02-26 00:02:26 +0530

    chore: add requirements file and update FastAPI app metadata

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,11 @@ from src.books.routes import book_router
 version = "v1"
 
 
-app = FastAPI(version=version)
+app = FastAPI(
+    title="Bookly",
+    description=" An app with REST API for a book review web service",
+    version=version,
+)
 
 
-app.include_router(book_router, prefix=f"/api/{version}/books")
+app.include_router(book_router, prefix=f"/api/{version}/books",tags=["books"])
