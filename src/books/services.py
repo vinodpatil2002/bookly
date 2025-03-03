3dee6b0103d645fa024d45caf1edc2fbd0cc7684
vinodpatil2002 <vinodnspatil@gmail.com>
2025-03-03 22:53:15 +0530

    feat: implement BookService methods for CRUD operations; add get_all_books, create_book, update_book, and delete_book

--- src/books/services.py
+++ src/books/services.py
@@ -1,20 +1,55 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
-from .schemas import BookCreateModel
+from .schemas import BookCreateModel, BookUpdateModel
+from sqlmodel import select, desc, delete
+from .models import Book
 
 
 class BookService:
     async def get_all_books(self, session: AsyncSession):
-        pass
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.exec(statement=statement)
+        return result.all()
 
-    async def get_book(self, book_uid, session: AsyncSession):
-        pass
+    async def get_book(self, book_uid: str, session: AsyncSession):
+        statement = select(Book).where(Book.uid == book_uid)
+
+        result = await session.exec(statement)
+
+        book = result.first()
+        if book is not None:
+            return book
+        else:
+            return None
 
     async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
-        pass
+        book_data_dict = book_data.model_dump()
+        new_book = Book(**book_data_dict)
+        session.add(new_book)
 
-    async def update_book(self, book_uid, session: AsyncSession):
-        pass
+        await session.commit()
+        return new_book
 
+    async def update_book(
+        self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession
+    ):
+        book_to_update = self.get_book(book_uid=book_uid, session=session)
+        if book_to_update is not None:
+            update_data_dict = update_data.model_dump()
+            for k, v in update_data_dict.items():
+                setattr(book_to_update, k, v)
 
-    async def update_book(self, book_uid, session: AsyncSession):
-        pass
+            await session.commit()
+
+            return book_to_update
+        else:
+            return None
+
+    async def delete_book(self, book_uid: str, session: AsyncSession):
+        book_to_delete = self.get_book(book_uid)
+        if book_to_delete is not None:
+            session.delete(book_to_delete)
+
+            await session.commit()
+
+        else:
+            return None
