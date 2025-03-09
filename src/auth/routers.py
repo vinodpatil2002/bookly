af7b219b79e21e0671be9b27841d29279ee77300
vinodpatil2002 <vinodnspatil@gmail.com>
2025-03-09 22:51:31 +0530

    feat: add logout route to authentication router

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -8,6 +8,12 @@ auth_router = APIRouter()
 async def create_user_account():
     pass
 
+
 @auth_router.post("/signin")
 async def login_user():
+    pass
+
+
+@auth_router.post("/signout")
+async def logout_user():
     pass
